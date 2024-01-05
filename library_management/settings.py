19d1707be65a75b229e564fc33dc31256cd52288
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2024-01-05 11:28:33 +0600

    added link

--- library_management/settings.py
+++ library_management/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-# CSRF_TRUSTED_ORIGINS = ['https://*.mydomain.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://library-management-49fk.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
